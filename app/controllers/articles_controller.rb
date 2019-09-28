ed6381f3383babc3e45a14f171674a06555f448b
yasmine <yasmine.ezequiel@gmail.com>
2019-09-28 23:30:47 +0200

    Adds comments and destroy comments, adds sign in basic function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  
   def index
     @articles = Article.all
   end
@@ -34,7 +36,14 @@ def update
     render 'edit'
   end
 end
+
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
  
+  redirect_to articles_path
+end
+
 private
 def article_params
   params.require(:article).permit(:title, :text)
